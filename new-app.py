d4533f69f25eeb7f48878b86f4f2b306ede883ea
Anschel Burk <anschel.burk@gmail.com>
2024-02-08 15:25:00 -0500

    Initial commit

---
+++ new-app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+# import numpy as np
+# import pandas as pd
+from daily_values import daily_values as dv
+
+# map_data = pd.DataFrame(
+#     np.random.randn(1000,2) / [50,50] + [37.76, -122.4],
+#     columns=['lat', 'lon'])
+
+# st.map(map_data)
+
+st.title('Nutrition Guide')
+search = st.text_input('Search an ingredient:')
+st.write(f'You searched: {search}')
+
+results = dv.get(search.lower())
+st.write(results)
