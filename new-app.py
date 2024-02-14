378be01fabc828a1805f154facdbd50e1616e733
ry_austin <rrarra@gmail.com>
2024-02-13 21:17:20 -0500

    Add USDA API integration for ingredient search  - use requests library to explore the search endpoint  - integrate results into streamlit front end

--- new-app.py
+++ new-app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 # import numpy as np
-import pandas as pd
+# import pandas as pd
+import requests
 from daily_values import daily_values as dv
+import requests
 
 # map_data = pd.DataFrame(
 #     np.random.randn(1000,2) / [50,50] + [37.76, -122.4],
@@ -9,11 +11,33 @@ from daily_values import daily_values as dv
 
 # st.map(map_data)
 
+DEMO_KEY = "DEMO_KEY"
+search_endpoint = "https://api.nal.usda.gov/fdc/v1/foods/search"
+
+
 st.title('Nutrition Guide')
 search = st.text_input('Search an ingredient:')
 st.write(f'You searched: {search}')
 
 st.title('Results:')
-results = pd.DataFrame.query(selfexpr=search.lower(), )
+r = requests.get(search_endpoint, params={"query": search, "api_key":DEMO_KEY}) # using the requests library to make a GET request to the API
+results = st.write(r.json().get('foods')[0].get('foodNutrients')) # getting the JSON object of the response and navigating to the foodNutrients key
+
+# further explanation of the code:
+# r.json() returns a JSON object of the response
+# r.json().get('foods') returns a list of foods
+# r.json().get('foods')[0] returns the first food in the list
+# r.json().get('foods')[0].get('foodNutrients') returns the foodNutrients of the first food in the list
+
+"""
+You can further use the api explorer here: https://app.swaggerhub.com/apis/fdcnal/food-data_central_api/1.0.1#/FDC/getFoodsSearch
+to see the different parameters you can use to customize your search and review the 'shape' of the response.
+"""
+
+
+
+
+# results = pd.DataFrame.query(selfexpr=search.lower(), )
 # results = dv.get(search.lower())
 # st.write(results)
+
