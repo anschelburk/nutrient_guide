75a954b7005722f239b7180fe0a7452bd1d94e5a
Bob Belderbos <bobbelderbos@gmail.com>
2024-03-12 13:11:15 +0100

    added 2nd test

--- test_nutrient_calculator.py
+++ test_nutrient_calculator.py
@@ -1,4 +1,4 @@
-from collections import defaultdict, Counter
+from collections import defaultdict
 
 import pytest
 
@@ -40,6 +40,22 @@ def test_nutrient_calculator(ncalc):
         float, {"Vitamin C": 25.0, "Vitamin A": 4.0, "Vitamin B": 12.0, "Iron": 16.0}
     )
     assert ncalc.target_ingredients == defaultdict(
-        float, {"Vitamin C": -25.0, "Vitamin A": -4.0, "Vitamin B": -12.0,
-                "Iron": -16.0}
+        float,
+        {"Vitamin C": -25.0, "Vitamin A": -4.0, "Vitamin B": -12.0, "Iron": -16.0},
     )
+
+
+def test_api_response(ncalc):
+    # note that we call the api here, for unit testing you might want to "mock"
+    # the api to not make a call over the network
+    ncalc.get_ingredients_for_nutrient("ORANGE")
+    assert len(ncalc.nutrient_ingredient_map) == 4
+    assert "orange" in ncalc.nutrient_ingredient_map
+    assert ncalc.nutrient_ingredient_map["orange"] == {
+        "Carbohydrate": {"unit": "G", "value": 90.5},
+        "Energy": {"unit": "KCAL", "value": 381},
+        "Protein": {"unit": "G", "value": 4.76},
+        "Sodium": {"unit": "MG", "value": 381},
+        "Total Sugars": {"unit": "G", "value": 90.5},
+        "Total lipid (fat)": {"unit": "G", "value": 0.0},
+    }
